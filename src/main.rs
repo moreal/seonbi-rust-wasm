a29748b37282121b4fafa57cc68554b79ce650b0
Lee Dogeon <dev.moreal@gmail.com>
2022-03-28 01:03:59 +0900

    feat: move to dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,30 @@
 mod services;
 
-use web_sys::HtmlTextAreaElement;
-use yew::prelude::*;
-use yew::{function_component, html, start_app, use_effect_with_deps, use_state, Callback};
+use dioxus::prelude::*;
 
-#[function_component(App)]
-fn app() -> Html {
-    let source = use_state(|| "선비 紹介 國漢文混用".to_string());
-    let result = use_state(|| "".to_string());
-    {
-        let source = source.clone();
-        let source_original = source.clone();
-        let result = result.clone();
-        use_effect_with_deps(
-            move |_| {
-                wasm_bindgen_futures::spawn_local(async move {
-                    let ret = services::seonbi::translate(&source).await.unwrap();
-                    result.set(ret);
-                });
-                || ()
+fn app(cx: Scope) -> Element {
+    let source = use_state(&cx, || "선비 紹介 國漢文混用".to_string());
+    let result = use_future(&cx, (source,), |(source,)| async move {
+        services::seonbi::translate(&source).await
+    });
+
+    cx.render(rsx! {
+        div {
+            textarea {
+                value: "{source}",
+                oninput: move |e| source.set(e.data.value.to_string())
             },
-            source_original,
-        );
-    }
-
-    let oninput = {
-        let source = source.clone();
-        Callback::from(move |e: yew::html::oninput::Event| {
-            if let Some(input) = e.target_dyn_into::<HtmlTextAreaElement>() {
-                source.set(input.value());
+            match result.value() {
+                Some(Ok(result)) => rsx! { h1 {
+                    "{result}"
+                }},
+                Some(Err(_)) => rsx! { div { "Error" } },
+                None => rsx! { div { "Maybe loading" } }
             }
-        })
-    };
-
-    html! {
-        <div>
-            <textarea value={source.to_string()} {oninput}/>
-            <h1>{ result.as_str() }</h1>
-        </div>
-    }
+        }
+    })
 }
 
 fn main() {
-    start_app::<App>();
+    dioxus::web::launch(app);
 }
