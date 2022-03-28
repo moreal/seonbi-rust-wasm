9edc7ab95de7079f06b23479280cf8777ad34026
Lee Dogeon <dev.moreal@gmail.com>
2022-03-28 22:09:31 +0900

    feat: apply css styling

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,24 @@ use dioxus::prelude::*;
 
 fn app(cx: Scope) -> Element {
     let source = use_state(&cx, || "".to_string());
-    let result = use_future(&cx, (source,), |(source,)| async move {
+    let result_fut = use_future(&cx, (source,), |(source,)| async move {
         services::seonbi::translate(&source).await
     });
+    let result = match result_fut.value() {
+        Some(Ok(result)) => result,
+        Some(Err(_)) => "Error",
+        None => "Maybe loading"
+    };
 
     cx.render(rsx! {
-        div {
-            textarea {
-                oninput: move |e| source.set(e.data.value.to_string())
-            },
-            match result.value() {
-                Some(Ok(result)) => rsx! { h1 {
-                    "{result}"
-                }},
-                Some(Err(_)) => rsx! { div { "Error" } },
-                None => rsx! { div { "Maybe loading" } }
-            }
+        style { [include_str!("../main.css")] }
+        textarea {
+            id: "translation-source",
+            oninput: move |e| source.set(e.data.value.to_string())
+        },
+        div {            
+            id: "translation-result",
+            "{result}"
         }
     })
 }
